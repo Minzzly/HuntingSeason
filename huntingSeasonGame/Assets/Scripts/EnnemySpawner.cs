756879678122cb4f07bcc411519fabd392a70d0e
Gabriel Gauthier <gab.gauthier2002@gmail.com>
2022-02-16 21:22:42 -0500

    ennemies can't spawn near the player

--- huntingSeasonGame/Assets/Scripts/EnnemySpawner.cs
+++ huntingSeasonGame/Assets/Scripts/EnnemySpawner.cs
@@ -21,7 +21,13 @@ public class EnnemySpawner : MonoBehaviour
     private IEnumerator spawnEnnemies(float interval, GameObject ennemy)
     {
         yield return new WaitForSeconds(interval);
-        Instantiate(ennemy, new Vector2(Random.Range(-10f, 10f), Random.Range(-6f, 6f)), Quaternion.identity);
+        Vector2 randomPos = new Vector2(Random.Range(-10f, 10f), Random.Range(-6f, 6f));
+        float distance = Vector2.Distance(transform.position, randomPos);
+        
+        if(distance >= 6f){
+            Instantiate(ennemy, randomPos, Quaternion.identity);
+        }
+        
         StartCoroutine(spawnEnnemies(interval, ennemy));
              
     }
